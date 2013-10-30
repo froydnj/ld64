66b1b8d856b9056b44f55a3404af5e435cce59e0
Nathan Froyd <froydnj@gmail.com>
2013-10-30 15:56:34 -0400

    fix implementation of strlcpy

--- src/linux-compat/strlcpy.c
+++ src/linux-compat/strlcpy.c
@@ -3,10 +3,28 @@
 #include "strlcpycat.h"
 
 size_t
-strlcpy(char* dst, const char* src, size_t size)
+strlcpy(char* dst, const char* src, size_t sz)
 {
-  dst[size - 1] = '\0';
-  strncpy(dst, src, size - 1);
-  return strlen(dst);
+  char *d = dst;
+  const char *s = src;
+  size_t n = sz;
+
+  /* Copy as many bytes as will fit */
+  if (n != 0) {
+    while (--n != 0) {
+      if ((*d++ = *s++) == '\0')
+	break;
+    }
+  }
+
+  /* Not enough room in dst, add NUL and traverse rest of src */
+  if (n == 0) {
+    if (siz != 0)
+      *d = '\0';		/* NUL-terminate dst */
+    while (*s++)
+      ;
+  }
+
+  return(s - src - 1);	/* count does not include NUL */
 }
 #endif
